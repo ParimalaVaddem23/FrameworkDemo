27ea913673fb7d2b8baac39cfc6eef3124047013
Parimala Vaddem <parimala.vaddem23@gmail.com>
2021-12-24 10:41:56 +0530

    Adding updated with class

--- src/test/java/com/ecommerce/qa/tests/SampleAutomation.java
+++ src/test/java/com/ecommerce/qa/tests/SampleAutomation.java
@@ -1,13 +1,25 @@
 package com.ecommerce.qa.tests;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class SampleAutomation {
     @Test
     public void verify_Assertion(){
-
+        WebDriver driver;
         System.out.println("Executing from Jenkins");
         Assert.assertEquals("Hello", "Hello");
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("headless");
+        options.addArguments("disable-gpu");
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver(options);
+        driver.get("https://www.google.com");
+        System.out.println("Title of the page is: " + driver.getTitle());
+        Assert.assertTrue(driver.getTitle().equals("Google"));
     }
 }
